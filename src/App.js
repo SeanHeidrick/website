80598c526d3b4504c0f9eb8bb68ba6f946ef4479
SeanHeidrick <seanheidrick1@gmail.com>
2020-04-21 18:48:05 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.scss";
+import { Pedals } from "./Pedals";
+import { NavBar } from "./NavBar";
+import { Welcome } from "./Welcome";
 
 function App() {
+  let pedalJSON = require("./pedalElement.json");
+  let navBarJSON = require("./navBar.json");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar navJSON={navBarJSON} />
+      <Welcome />
+      <Pedals pedalJ={pedalJSON} />
+    </>
   );
 }
-
 export default App;
